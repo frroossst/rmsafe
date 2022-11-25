aacf7cd6f9d936d41aff39866cf7030e8c3db962
Adhyan H <adhyanpatel@gmail.com>
2022-11-24 20:51:42 -0600

    working on pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use std::{process::Command, path::Path};
+use std::{process::Command, path::{Path, PathBuf}};
 use clap::Parser;
+use glob::glob;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
 struct Args
     {
     /// the file to be removed
-    cmd: String,
+    #[clap(short, long, value_parser)]
+    cmd: Option<String>,
+
     /// wildcard expressions matching 
+    #[clap(short, long, value_parser)]
     rgex: Option<String>,
     }
 
@@ -15,18 +19,43 @@ fn main()
     {
     let args = Args::parse();
 
-    let trashcan_path = Path::new("/home/home/.local/share/Trash/files");
-
     println!("saferm: powered with <3 by Rust");
 
-    let cmd = Command::new("mv")
-        .arg("./".to_string() + &args.cmd.to_owned())
+    // TODO: iterate and rm
+    // let mut ls_cmd = Command::new("ls");
+    // let ls_output = ls_cmd.spawn().unwrap();
+    // let a = ls_output.stdout;
+    // println!("{:?}", a);
+
+    for entry in glob(&args.rgex.unwrap()).unwrap()
+        {
+        match entry
+            {
+            Ok(p) => 
+                { 
+                println!("{:?}", p.display()); 
+                move_to_trash(p);
+                },
+            Err(e) => { println!("{:?}", e); },
+            }
+        }
+
+
+    }
+
+fn move_to_trash(file_to_be_trashed: PathBuf)
+    {
+    // TODO: make username variable
+    let trashcan_path = Path::new("/home/home/.local/share/Trash/files");
+
+    let mv_cmd = Command::new("mv")
+        .arg(&file_to_be_trashed.to_owned())
         .arg(trashcan_path)
         .spawn();
 
-    match cmd
+    match mv_cmd
         {
-        Ok(_) => { cmd.unwrap(); },
-        Err(e) => { println!("{:?}", e); }
+        Ok(_) => { mv_cmd.unwrap(); },
+        Err(e) => { eprintln!("{:?}", e); }
         }
     }
